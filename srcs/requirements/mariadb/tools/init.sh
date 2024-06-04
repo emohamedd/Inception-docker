3ba757197ae3c3b127b11c5fd8356efeb3c0ec2e
a.elgaouej@gmail.com <a.elgaouej@gmail.com>
2024-06-04 00:30:42 -0700

    mariadb : done

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+
+echo "FLUSH PRIVILEGES;" > init.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> init.sql
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> init.sql
+
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> init.sql
+
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';" >> init.sql
+
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+mariadbd -u mysql --bootstrap < init.sql
+
+rm init.sql
+
+exec "$@"
